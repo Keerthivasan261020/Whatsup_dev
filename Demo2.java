50cce8d779f17edb161af88760078f3d717b0d94
Keerthivasan261020 <rkv00773@gmail.com>
2025-02-03 21:08:00 +0530

    loop code added

---
+++ Demo2.java
@@ -0,0 +1,11 @@
+class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		int [] arr={205,204,203,202,201,200};
+		for(int i =arr.length-1; i>=0; i--)
+		{
+		System.out.println(arr[i]);
+		}
+	}
+}
